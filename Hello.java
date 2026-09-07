970daf02a862d4cdd9e360b8eafa9d5b2e454304
hasini <hasinidumala@gmail.com>
2026-09-07 05:41:33 -0400

    Initial Java application

---
+++ Hello.java
@@ -0,0 +1,6 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello from Jenkins!");
+        System.out.println("Java application built successfully.");
+    }
+}
